53d03499429795dc55b6eeeb74534b773ebf6d83
Harsh Pandit <harsh.pandit2004@gmail.com>
2022-11-03 23:09:00 +0530

    it finally switches...

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import BMinit from "./Components/BMinit";
 function App() {
   const [something, setsomething] = useState();
   const [initval, setInitval] = useState(true);
-
+  const [temp, setTemp] = useState(true);
+  
   useEffect(() => {
     fetch("https://batsignal-backend.herokuapp.com/SignalValue", {
       method: "GET",
@@ -15,30 +16,33 @@ function App() {
         console.log(data);
         setsomething(data.stuff[0].signalValue);
       });
-  }, []);
+  }, [temp]);
 
   const switchhandler = () => {
     fetch(
       "https://batsignal-backend.herokuapp.com/patchasignalvalue/6357e3e3d837d737e22d7ee4",
       {
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        method: "PATCH"
+        method: "PATCH",
+        mode:"cors",
+        headers: { "Content-type": "text/plain" },
+        data: JSON.stringify()
       }
-    ).then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-    })
+    )
+      .then((res) => {
+        console.log(res.status);
+      })
+      .then((data) => {
+        console.log(data);
+      });
     setInitval(false);
+    setTemp(!temp);
   };
-
+  
   return (
     <div className="App">
       <header className="App-header">
         {initval ? <BMinit /> : <BatCR something={something} />}
-        <button className="toggleswitch" onClick={switchhandler}>
+        <button type="button" className="toggleswitch" onClick={switchhandler}>
           Switch
         </button>
       </header>
